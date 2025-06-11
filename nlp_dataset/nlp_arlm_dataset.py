2538952ea36eaff2bac02af26f9a2c60c0f83e18
Nicolai Müller <nicolaimu1212@gmail.com>
2025-06-11 16:51:46 +0200

    Add arlm dataset generation

--- nlp_dataset/nlp_arlm_dataset.py
+++ nlp_dataset/nlp_arlm_dataset.py
@@ -3,8 +3,14 @@ from typing import Tuple, List
 from nlp_dataset import NLPDataset
 
 class NLPARLMDataset(NLPDataset):
-    def __init__(self, input_ids: List[List[int]]):
+    def __init__(self, input_ids: List[List[int]], block_size: int):
         super().__init__(input_ids)
+
+        ids = []
+        for sequence in self.input_ids:
+            ids.extend(sequence)
+
+        self.input_ids = [ids[i : i + block_size + 1] for i in range(0, len(ids) - block_size, block_size)]
 
     def __getitem__(self, index: int) -> Tuple[List[int], List[int]]:
         input_ids = self.input_ids[index]
