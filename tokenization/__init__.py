00f833badfca405813f7f70878f54e387b0cc7f8
Nicolai Müller <nicolaimu1212@gmail.com>
2025-06-11 14:31:14 +0200

    Add character_tokenizer.py

--- tokenization/__init__.py
+++ tokenization/__init__.py
@@ -1,2 +1,3 @@
 from .tokenizer import Tokenizer
 from .word_tokenizer import WordTokenizer
+from .character_tokenizer import CharacterTokenizer
--- tokenization/character_tokenizer.py
+++ tokenization/character_tokenizer.py
@@ -1,9 +1,9 @@
-import re
+from typing import List
 
 from tokenization.tokenizer import Tokenizer
 from collections import Counter
 
-class WordTokenizer(Tokenizer):
+class CharacterTokenizer(Tokenizer):
     def __init__(self, dataset: list[str], vocab_size: int=10_000, max_length: int=-1,
                  unknown_token: str='<UNK>', pad_token: str='<PAD', special_tokens=None):
 
@@ -15,14 +15,14 @@ class WordTokenizer(Tokenizer):
     def _build_vocab(self, dataset: list[str]):
         value_counts = Counter()
         for sequence in dataset:
-            tokens = re.findall(r"\w+(?:'\w+)?|[^\w\s]", sequence.lower())
-            value_counts.update(tokens)
+            for character in sequence:
+                value_counts.update(character)
 
-        for word, _ in value_counts.most_common(self.vocab_size - len(self.vocab)):
-            self.vocab[word] = len(self.vocab)
+        for character, _ in value_counts.most_common(self.vocab_size - len(self.vocab)):
+            self.vocab[character] = len(self.vocab)
 
 
-    def __call__(self, text: list[str]) -> list[list[int]]:
+    def __call__(self, text: List[str]) -> List[List[int]]:
         tokens = []
         for sequence in text:
             tokens.append(self.tokenize(sequence))
@@ -32,13 +32,10 @@ class WordTokenizer(Tokenizer):
 
     def tokenize(self, text: str) -> list[int]:
         tokens = []
-        for word in re.findall(r"\w+(?:'\w+)?|[^\w\s]", text.lower()):
-            tokens.append(self.vocab.get(word, self.vocab[self.unknown_token]))
+        for character in text:
+            tokens.append(self.vocab.get(character, self.vocab[self.unknown_token]))
 
-        if self.max_length == -1:
-            return tokens
-        else:
-            return tokens[:self.max_length]
+        return tokens
 
     def get_pad_token(self) -> int:
         return self.vocab[self.pad_token]
