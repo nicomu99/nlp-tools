1c0be320202d670025c1b8e53e65960ed379dbca
Nicolai Müller <nicolaimu1212@gmail.com>
2025-06-18 15:11:00 +0200

    Add arlm logging

--- logger/arlm_logger.py
+++ logger/arlm_logger.py
@@ -4,3 +4,26 @@ class ARLMLogger(Logger):
     def __init__(self, run_name):
         super().__init__(run_name)
 
+        self.loss = 0
+        self.processed = 0
+
+    def init_epoch(self):
+        super().init_epoch()
+
+        self.loss = 0
+        self.processed = 0
+
+    def update(self, outputs, targets, loss):
+        super().update_memory()
+
+        batch_size = targets.size(0)
+        self.processed += batch_size
+        self.loss += loss.item() * batch_size
+
+    def log_epoch(self, stage):
+        metric_dir = {
+            'epoch': self.epoch,
+            'loss': self.loss / self.processed
+        }
+
+        self.save_metrics_and_memory(metric_dir, stage)
