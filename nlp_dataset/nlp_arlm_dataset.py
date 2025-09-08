4e6079606f22ac8c165a89a6db42dbd3b6d28b10
Nicolai Müller <nicolaimue@gmail.com>
2025-09-08 19:38:06 +0200

    Fix dataset

--- nlp_dataset/nlp_arlm_dataset.py
+++ nlp_dataset/nlp_arlm_dataset.py
@@ -6,7 +6,11 @@ class NLPARLMDataset(NLPDataset):
     def __init__(self, input_ids: List[List[int]], block_size: int):
         super().__init__(input_ids)
 
-        self.input_ids = input_ids
+        flattened_text = []
+        for input_id in input_ids:
+            flattened_text.extend(input_id)
+
+        self.input_ids = [flattened_text[i:i + block_size] for i in range(0, len(flattened_text), block_size)]
 
     def __getitem__(self, index: int) -> Tuple[List[int], List[int]]:
         input_ids = self.input_ids[index]
